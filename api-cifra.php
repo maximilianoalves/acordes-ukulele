3b77d6e1906a001200b86535890d5d7c155b4649
Maximiliano <maximilianodacruz@gmail.com>
2016-07-21 22:37:23 -0300

    performance

--- api-cifra.php
+++ api-cifra.php
@@ -16,8 +16,7 @@ function montPageHome($nota){
 	$types = array("major", "minor", "aug", "dim", "7", "m7", "maj7", "m7b5", "sus2", "sus4", "7sus4", "6", "m6", "add9", "m9", "9", "11", "13");
 
 	foreach ($types as $value) {
-		$api_url = 'http://www.ukulele-chords.com/get?ak=d41d8cd98f00b204e9800998ecf8427e&r='.$nota.'&typ='.$value;
-		getFirstChord($api_url, $value, $nota);
+		getFirstChord($value, $nota);
 	}
 }
 
@@ -52,14 +51,14 @@ function getImageChord($api_url){
 	//$res = $chord->chord_name_photo;
 }
 
-function getFirstChord($api_url, $value, $nota){
+function getFirstChord($value, $nota){
 	//XML file to load
 	//converts the specified XML file into a SimpleXMLElement object
-	$xml = simplexml_load_file($api_url);
+	//$xml = simplexml_load_file($api_url);
 	//Navigate through the tree to get your info (chord diagram here)
 	//chord[0] returns the "main chord". For alternative positions, use a foreach() loop.
 	//$res = $chord->chord_diag_mini;
-	$res = $xml->chord[0]->chord_name;
+	$res = $nota.$value;
 	//$res = $chord->chord_photo;
 	echo '<a href="nota.php?nota='.$nota.'&type='.$value.'">'.$res.'</a> ';
 	//echo '<br>';
